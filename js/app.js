4cbb82b8f83365e7179486c577fcaa1aa983b14c
minischetti <domminischetti@gmail.com>
2017-03-02 17:13:09 -0500

    Remove old styles, Add background support and audio support when a hero is chosen, Create hero JS objects, Add image and sound functions

--- js/app.js
+++ js/app.js
@@ -1,12 +1,24 @@
-heroes = ['Genji', 'McCree'];
+var heroNames = ["Genji", "McCree"];
+var heroes = [genji, mccree];
+
+var genji = {
+  name: "Genji",
+  line: "Mada mada."
+}
+var mccree = {
+  name: "McCree",
+  line: "It's high noon somewhere in the world.",
+  image: "url('assets/mccree.jpg')",
+  sound: "assets/mccree.ogg"
+}
 
 selectedHero = '';
+var heroMenu = document.getElementById('heroMenu');
 
 for (var i = 0; i < heroes.length; i++) {
-  var heroMenu = document.getElementById('heroMenu');
   var hero = document.createElement('option');
   heroMenu.appendChild(hero);
-  hero.value, hero.text = heroes[i];
+  hero.value, hero.text = heroNames[i];
 }
 
 heroMenu.addEventListener('change', updateHero);
@@ -17,11 +29,26 @@ function updateHero() {
   yourResults();
 }
 
+function createBackground(hero) {
+  var background = document.createElement('div');
+  document.body.appendChild(background);
+  background.id = 'image';
+  var image = document.getElementById('image');
+  image.style.backgroundImage = hero.image;
+}
+
+function playSound(hero) {
+  var playSound = new Audio(hero.sound);
+  playSound.play();
+}
+
 function yourResults() {
   switch (selectedHero) {
     case 'McCree':
     if (selectedHero === 'McCree') {
-      alert("It's high noon somewhere in the world.");
+      alert(mccree.line);
+      createBackground(mccree);
+      playSound(mccree);
     }
     break;
   }
