e32eacd5d5eb8d410b055d530140f37068c40780
minischetti <domminischetti@gmail.com>
2017-03-02 19:07:17 -0500

    Change behavior to update existing elements rather than continually creating new ones, Add result and name when hero is chosen

--- js/app.js
+++ js/app.js
@@ -1,15 +1,23 @@
-var heroNames = ["Genji", "McCree"];
-var heroes = [genji, mccree];
+var heroNames = ["Choose", "Genji", "McCree"];
+var heroes = [choose, genji, mccree];
+
+var choose = {
+  name: "Choose a Hero"
+}
 
 var genji = {
   name: "Genji",
-  line: "Mada mada."
+  line: "Mada mada.",
+  image: "url('assets/genji.jpg')",
+  sound: "assets/genji.ogg",
+  color: "#96ee42"
 }
 var mccree = {
   name: "McCree",
-  line: "It's high noon somewhere in the world.",
+  line: "When you call high noon everyone steps outside.",
   image: "url('assets/mccree.jpg')",
-  sound: "assets/mccree.ogg"
+  sound: "assets/mccree.ogg",
+  color: "#af595c"
 }
 
 selectedHero = '';
@@ -30,11 +38,34 @@ function updateHero() {
 }
 
 function createBackground(hero) {
-  var background = document.createElement('div');
-  document.body.appendChild(background);
-  background.id = 'image';
+  // var background = document.createElement('div');
+  // document.body.appendChild(background);
+  // background.id = 'image';
   var image = document.getElementById('image');
   image.style.backgroundImage = hero.image;
+}
+
+function tint(hero) {
+  // var tint = document.createElement('div');
+  // document.body.appendChild(tint);
+  // tint.id = 'tint';
+  var tint = document.getElementById('tint');
+  tint.style.backgroundColor = hero.color;
+}
+
+function heroResult(hero) {
+  // var title = document.createElement('h1');
+  // document.body.appendChild(title);
+  // title.id = 'line';
+  var result = document.getElementById('heroResult');
+  result.innerHTML = hero.line;
+}
+function heroName(hero) {
+  // var title = document.createElement('h1');
+  // document.body.appendChild(title);
+  // title.id = 'line';
+  var heroName = document.getElementById('heroName');
+  heroName.innerHTML = hero.name;
 }
 
 function playSound(hero) {
@@ -44,11 +75,22 @@ function playSound(hero) {
 
 function yourResults() {
   switch (selectedHero) {
+    case 'Genji':
+    if (selectedHero === 'Genji') {
+      createBackground(genji);
+      tint(genji);
+      playSound(genji);
+      heroResult(genji);
+      heroName(genji);
+    }
+    break;
     case 'McCree':
     if (selectedHero === 'McCree') {
-      alert(mccree.line);
       createBackground(mccree);
+      tint(mccree);
       playSound(mccree);
+      heroResult(mccree);
+      heroName(mccree);
     }
     break;
   }
