6d27010ccfe1a49c33943c1edc028713df5b9e66
minischetti <domminischetti@gmail.com>
2017-03-05 13:13:02 -0500

    Add new images, Add Big Noodle web fonts, Add Pharah and Reaper, Remove Choose Hero, Add JS animation support, Improve overall styles

--- js/app.js
+++ js/app.js
@@ -1,9 +1,5 @@
-var heroNames = ["Choose", "Genji", "McCree"];
-var heroes = [choose, genji, mccree];
-
-var choose = {
-  name: "Choose a Hero"
-}
+var heroNames = ["Genji", "McCree", "Pharah", "Reaper"];
+var heroes = [genji, mccree, pharah, reaper];
 
 var genji = {
   name: "Genji",
@@ -18,6 +14,20 @@ var mccree = {
   image: "url('assets/mccree.jpg')",
   sound: "assets/mccree.ogg",
   color: "#af595c"
+}
+var pharah = {
+  name: "Pharah",
+  line: ["A team wipe is in your future.", "Environmental kills are your friend."],
+  image: "url('assets/pharah.jpg')",
+  sound: "assets/mccree.ogg",
+  color: "#3c7ac5"
+}
+var reaper = {
+  name: "Reaper",
+  line: ["Horrible aim is your curse.", "Tanks stand no chance against your shotguns."],
+  image: "url('assets/reaper.jpg')",
+  sound: "assets/mccree.ogg",
+  color: "#7d3e51"
 }
 
 selectedHero = '';
@@ -35,34 +45,63 @@ heroMenu.addEventListener('change', updateHero);
 function updateHero() {
   selectedHero = heroMenu.value;
   console.log(selectedHero);
+  page = document.getElementById('heroPage');
+  view.yourResults();
+}
+
+function randomHero() {
+  selectedHero = heroNames[Math.floor(Math.random() * heroNames.length)];
+  console.log(selectedHero);
+  page = document.getElementById('heroPage');
   view.yourResults();
 }
 
 //make first div selected, if one exists edit hidden one, not visible then move visible one out
-
 var view = {
   createBackground: function(hero) {
     var image = document.getElementById('image');
     var tint = document.getElementById('tint');
-    image.style.backgroundImage = hero.image;
-    tint.style.backgroundColor = hero.color;
+    if (image.classList.contains('animate')) {
+      image.classList.remove('animate');
+      tint.classList.remove('animate');
+    }
+    setTimeout(function() {
+      image.classList.add('animate');
+      tint.classList.add('animate');
+      image.style.backgroundImage = hero.image;
+      tint.style.backgroundColor = hero.color;
+    }, 1000);
   },
   heroResult: function(hero) {
     var result = document.getElementById('heroResult');
     var randomResult = hero.line[Math.floor(Math.random() * hero.line.length)];
-    result.innerHTML = randomResult;
+    if (result.classList.contains('animate')) {
+      result.classList.remove('animate');
+    }
+    setTimeout(function() {
+      result.classList.add('animate');
+      result.innerHTML = randomResult;
+    }, 1000);
   },
   heroName: function(hero) {
     var heroName = document.getElementById('heroName');
-    heroName.innerHTML = hero.name;
+    if (heroName.classList.contains('animate')) {
+      heroName.classList.remove('animate');
+    }
+    setTimeout(function() {
+      heroName.classList.add('animate');
+      heroName.innerHTML = hero.name;
+    }, 1000);
   },
   playSound: function(hero) {
-    var playSound = new Audio(hero.sound);
-    playSound.play();
+    setTimeout(function() {
+      var playSound = new Audio(hero.sound);
+      playSound.play();
+    }, 1000);
   },
   generateHeroPage: function(hero) {
     view.createBackground(hero);
-    view.playSound(hero);
+    //view.playSound(hero);
     view.heroResult(hero);
     view.heroName(hero);
   },
@@ -76,6 +115,16 @@ var view = {
       case 'McCree':
       if (selectedHero === 'McCree') {
         view.generateHeroPage(mccree);
+      }
+      break;
+      case 'Pharah':
+      if (selectedHero === 'Pharah') {
+        view.generateHeroPage(pharah);
+      }
+      break;
+      case 'Reaper':
+      if (selectedHero === 'Reaper') {
+        view.generateHeroPage(reaper);
       }
       break;
     }
