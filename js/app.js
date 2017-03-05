c9a5b6114c52b79c336533bcd40260a84204c1d8
minischetti <domminischetti@gmail.com>
2017-03-05 15:41:06 -0500

    Add all heroes, Fix crossfade effect on image change

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
-var heroNames = ["Genji", "McCree", "Pharah", "Reaper"];
-var heroes = [genji, mccree, pharah, reaper];
+var heroNames = ["Genji", "McCree", "Pharah", "Reaper", "Soldier: 76", "Sombra", "Tracer", "Bastion", "Hanzo", "Junkrat", "Mei", "Torbjorn", "Widowmaker", "D.VA", "Reinhardt", "Roadhog", "Winston", "Zarya", "Ana", "Lucio", "Mercy", "Symmetra", "Zenyatta"];
+var heroes = [genji, mccree, pharah, reaper, soldier76, sombra, tracer, bastion, hanzo, junkrat, mei, torbjorn, widowmaker, dva, reinhardt, roadhog, winston, zarya, ana, lucio, mercy, symmetra, zenyatta];
 
 var genji = {
   name: "Genji",
@@ -28,6 +28,139 @@ var reaper = {
   image: "url('assets/reaper.jpg')",
   sound: "assets/mccree.ogg",
   color: "#7d3e51"
+}
+var soldier76 = {
+  name: "Soldier: 76",
+  line: ["Horrible aim is your curse.", "Tanks stand no chance against your shotguns."],
+  image: "url('assets/soldier.jpg')",
+  sound: "assets/mccree.ogg",
+  color: "#6b7996"
+}
+var sombra = {
+  name: "Sombra",
+  line: ["Horrible aim is your curse.", "Tanks stand no chance against your shotguns."],
+  image: "url('assets/sombra.jpg')",
+  sound: "assets/mccree.ogg",
+  color: "#7258b9"
+}
+var tracer = {
+  name: "Tracer",
+  line: ["Horrible aim is your curse.", "Tanks stand no chance against your shotguns."],
+  image: "url('assets/tracer.jpg')",
+  sound: "assets/mccree.ogg",
+  color: "#d79342"
+}
+var bastion = {
+  name: "Bastion",
+  line: ["Horrible aim is your curse.", "Tanks stand no chance against your shotguns."],
+  image: "url('assets/bastion.jpg')",
+  sound: "assets/mccree.ogg",
+  color: "#7d907c"
+}
+var hanzo = {
+  name: "Hanzo",
+  line: ["Horrible aim is your curse.", "Tanks stand no chance against your shotguns."],
+  image: "url('assets/hanzo.jpg')",
+  sound: "assets/mccree.ogg",
+  color: "#bab28b"
+}
+var junkrat = {
+  name: "Junkrat",
+  line: ["Horrible aim is your curse.", "Tanks stand no chance against your shotguns."],
+  image: "url('assets/junkrat.jpg')",
+  sound: "assets/mccree.ogg",
+  color: "#e9ba52"
+}
+var mei = {
+  name: "Mei",
+  line: ["Horrible aim is your curse.", "Tanks stand no chance against your shotguns."],
+  image: "url('assets/mei.jpg')",
+  sound: "assets/mccree.ogg",
+  color: "#6cacec"
+}
+var torbjorn = {
+  name: "Torbjorn",
+  line: ["Horrible aim is your curse.", "Tanks stand no chance against your shotguns."],
+  image: "url('assets/torbjorn.jpg')",
+  sound: "assets/mccree.ogg",
+  color: "#c1746e"
+}
+var widowmaker = {
+  name: "Widowmaker",
+  line: ["Horrible aim is your curse.", "Tanks stand no chance against your shotguns."],
+  image: "url('assets/widowmaker.jpg')",
+  sound: "assets/mccree.ogg",
+  color: "#9e6aa8"
+}
+var dva = {
+  name: "D.VA",
+  line: ["Horrible aim is your curse.", "Tanks stand no chance against your shotguns."],
+  image: "url('assets/dva.jpg')",
+  sound: "assets/mccree.ogg",
+  color: "#ed93c9"
+}
+var reinhardt = {
+  name: "Reinhardt",
+  line: ["Horrible aim is your curse.", "Tanks stand no chance against your shotguns."],
+  image: "url('assets/reinhardt.jpg')",
+  sound: "assets/mccree.ogg",
+  color: "#95a0a6"
+}
+var roadhog = {
+  name: "Roadhog",
+  line: ["Horrible aim is your curse.", "Tanks stand no chance against your shotguns."],
+  image: "url('assets/roadhog.jpg')",
+  sound: "assets/mccree.ogg",
+  color: "#b3894f"
+}
+var winston = {
+  name: "Winston",
+  line: ["Horrible aim is your curse.", "Tanks stand no chance against your shotguns."],
+  image: "url('assets/winston.jpg')",
+  sound: "assets/mccree.ogg",
+  color: "#9ea3b9"
+}
+var zarya = {
+  name: "Zarya",
+  line: ["Horrible aim is your curse.", "Tanks stand no chance against your shotguns."],
+  image: "url('assets/zarya.jpg')",
+  sound: "assets/mccree.ogg",
+  color: "#e97eb6"
+}
+var ana = {
+  name: "Ana",
+  line: ["Horrible aim is your curse.", "Tanks stand no chance against your shotguns."],
+  image: "url('assets/ana.jpg')",
+  sound: "assets/mccree.ogg",
+  color: "#6f88b1"
+}
+var lucio = {
+  name: "Lucio",
+  line: ["Horrible aim is your curse.", "Tanks stand no chance against your shotguns."],
+  image: "url('assets/lucio.jpg')",
+  sound: "assets/mccree.ogg",
+  color: "#85c952"
+}
+var mercy = {
+  name: "Mercy",
+  line: ["Horrible aim is your curse.", "Tanks stand no chance against your shotguns."],
+  image: "url('assets/mercy.jpg')",
+  sound: "assets/mccree.ogg",
+  color: "#ece9bc"
+}
+var symmetra = {
+  name: "Symmetra",
+  line: ["Horrible aim is your curse.", "Tanks stand no chance against your shotguns."],
+  image: "url('assets/symmetra.jpg')",
+  sound: "assets/mccree.ogg",
+  color: "#8ebbce"
+}
+var zenyatta = {
+  name: "Zenyatta",
+  line: ["Horrible aim is your curse.", "Tanks stand no chance against your shotguns."],
+  image: "url('assets/zenyatta.jpg')",
+  sound: "assets/mccree.ogg",
+  color: "#ebe380"
 }
 
 selectedHero = '';
@@ -125,6 +258,101 @@ var view = {
       case 'Reaper':
       if (selectedHero === 'Reaper') {
         view.generateHeroPage(reaper);
+      }
+      break;
+      case 'Soldier: 76':
+      if (selectedHero === 'Soldier: 76') {
+        view.generateHeroPage(soldier76);
+      }
+      break;
+      case 'Sombra':
+      if (selectedHero === 'Sombra') {
+        view.generateHeroPage(sombra);
+      }
+      break;
+      case 'Tracer':
+      if (selectedHero === 'Tracer') {
+        view.generateHeroPage(tracer);
+      }
+      break;
+      case 'Bastion':
+      if (selectedHero === 'Bastion') {
+        view.generateHeroPage(bastion);
+      }
+      break;
+      case 'Hanzo':
+      if (selectedHero === 'Hanzo') {
+        view.generateHeroPage(hanzo);
+      }
+      break;
+      case 'Junkrat':
+      if (selectedHero === 'Junkrat') {
+        view.generateHeroPage(junkrat);
+      }
+      break;
+      case 'Mei':
+      if (selectedHero === 'Mei') {
+        view.generateHeroPage(mei);
+      }
+      break;
+      case 'Torbjorn':
+      if (selectedHero === 'Torbjorn') {
+        view.generateHeroPage(torbjorn);
+      }
+      break;
+      case 'Widowmaker':
+      if (selectedHero === 'Widowmaker') {
+        view.generateHeroPage(widowmaker);
+      }
+      break;
+      case 'D.VA':
+      if (selectedHero === 'D.VA') {
+        view.generateHeroPage(dva);
+      }
+      break;
+      case 'Reinhardt':
+      if (selectedHero === 'Reinhardt') {
+        view.generateHeroPage(reinhardt);
+      }
+      break;
+      case 'Roadhog':
+      if (selectedHero === 'Roadhog') {
+        view.generateHeroPage(roadhog);
+      }
+      break;
+      case 'Winston':
+      if (selectedHero === 'Winston') {
+        view.generateHeroPage(winston);
+      }
+      break;
+      case 'Zarya':
+      if (selectedHero === 'Zarya') {
+        view.generateHeroPage(zarya);
+      }
+      break;
+      case 'Ana':
+      if (selectedHero === 'Ana') {
+        view.generateHeroPage(ana);
+      }
+      break;
+      case 'Lucio':
+      if (selectedHero === 'Lucio') {
+        view.generateHeroPage(lucio);
+      }
+      break;
+      case 'Mercy':
+      if (selectedHero === 'Mercy') {
+        view.generateHeroPage(mercy);
+      }
+      break;
+      case 'Symmetra':
+      if (selectedHero === 'Symmetra') {
+        view.generateHeroPage(symmetra);
+      }
+      break;
+      case 'Zenyatta':
+      if (selectedHero === 'Zenyatta') {
+        view.generateHeroPage(zenyatta);
       }
       break;
     }
